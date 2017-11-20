d3a4f07f961f0e856c582b02ba606882da2b4127
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-11-19 16:16:01 -0800

    updated drop downs to option elements, completed matching functionality. Needs CSS file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,164 @@
 var friendsList = [
   {
-    name: "Mark",
-    photo: "mark's photo",
+    name: "Joe",
+    photo: "https://s2-prod.mirror.co.uk/@trinitymirrordigital/chameleon-partnership-widgets/weather/img/grey/29.png",
     scores: [
-    	4,
-    	3,
-    	5,
-    	1,
-    	3
+    	"1",
+    	"2",
+    	"3",
+    	"5",
+    	"3",
+    	"2",
+    	"5",
+    	"4",
+    	"4",
+    	"5"
     ]
-  }
+  },
+  {
+    name: "Beth",
+    photo: "https://s2-prod.cambridge-news.co.uk/@trinitymirrordigital/chameleon-partnership-widgets/weather/img/grey/12.png",
+    scores: [
+    	"2",
+    	"2",
+    	"4",
+    	"2",
+    	"3",
+    	"5",
+    	"3",
+    	"4",
+    	"1",
+    	"5"
+    ]
+  },
+  {
+    name: "Sam",
+    photo: "https://s2-prod.irishmirror.ie/@trinitymirrordigital/chameleon-partnership-widgets/weather/img/grey/27.png",
+    scores: [
+    	"5",
+    	"3",
+    	"2",
+    	"4",
+    	"1",
+    	"5",
+    	"3",
+    	"4",
+    	"1",
+    	"2"
+    ]
+  },
+  {
+    name: "Mort",
+    photo: "https://s2-prod.irishmirror.ie/@trinitymirrordigital/chameleon-partnership-widgets/weather/img/grey/33.png",
+    scores: [
+    	"2",
+    	"3",
+    	"1",
+    	"4",
+    	"3",
+    	"2",
+    	"5",
+    	"5",
+    	"2",
+    	"1"
+    ]
+  },
+  {
+    name: "Olga",
+    photo: "http://www.observerbd.com/2016/09/29/1475079729.jpg",
+    scores: [
+    	"4",
+    	"1",
+    	"5",
+    	"5",
+    	"5",
+    	"2",
+    	"2",
+    	"1",
+    	"1",
+    	"3"
+    ]
+  },
+  {
+    name: "Jane",
+    photo: "https://s3.amazonaws.com/media.muckrack.com/mrdaily/images/2014/05/20/journalistwoman.png.700x700_q85_autocrop.png",
+    scores: [
+    	"5",
+    	"1",
+    	"2",
+    	"3",
+    	"4",
+    	"5",
+    	"4",
+    	"3",
+    	"2",
+    	"1"
+    ]
+  },
+  {
+    name: "Xander",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Zombie-156055.svg/512px-Zombie-156055.svg.png",
+    scores: [
+    	"2",
+    	"2",
+    	"4",
+    	"4",
+    	"3",
+    	"3",
+    	"3",
+    	"3",
+    	"3",
+    	"3"
+    ]
+  },
+  {
+    name: "Millie",
+    photo: "https://pbs.twimg.com/profile_images/655643418480111616/h1WZaTtp_400x400.jpg",
+    scores: [
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"
+    ]
+  },
+  {
+    name: "Ben",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScZJumqizpZMgkzIJ0u06TVKn1rkOXCFBnWRixurWVD2mNDXzj",
+    scores: [
+        "1",
+        "3",
+        "1",
+        "3",
+        "1",
+        "3",
+        "1",
+        "3",
+        "1",
+        "3"
+    ]
+  },
+  {
+    name: "Kalluie",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/613v0krW1qL._SL1500_.jpg",
+    scores: [
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4"
+    ]
+  },
 ];
 
 module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,4 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information from friendsList
-// ===============================================================================
-
-var friends = require("../data/friends");
-
-
+var friendsList = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
@@ -15,41 +7,68 @@ var friends = require("../data/friends");
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    res.json(friendsList);
   });
-
-
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the friendsList array)
-  // ---------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-      friends.push(req.body);
-      res.json(true);
+    friendsList.unshift(req.body);
+    res.json(true);
+    
+    var newestUser = friendsList[0]
+    var compareResults = [];
 
-  });
+    for (var i = 1; i < friendsList; i++) {
+      var differenceArray = []
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+      for (var j = 0; j < 10; j++){
+        var diff = Math.abs(newestUser.scores[j] - friendsList[i].scores[j])
+        differenceArray.push(diff)
+      }
 
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friends = [];
+      friendsList[i].compare = differenceArray.reduce(function(sum, value) {
+        return sum + value;
+      }, 1);
+      compareResults.push(friendsList[i].compare)
+    }
 
-    console.log(friends);
+    var sortArray = compareResults.sort(function(a, b){
+      return (a - b)
+    })   
+
+    var bestMatch 
+
+    for (var i = 1; i < friendsList.length; i++){
+      if (sortArray[0] === friendsList[i].compare){
+        bestMatch = friendsList[i]
+      }
+    }
+
+    var index = friendsList.indexOf(bestMatch)
+
+    function sort(){
+      friendsList.splice(index, 1)
+      friendsList.splice(1, 0, bestMatch)
+    }
+    sort();
+    
+    var keepKeys = ['name', 'photo', 'scores'];
+
+    for(var i = 0; i < friendsList.length; i++){
+
+      for(var key in friendsList[i]){
+        if(keepKeys.indexOf(key) === -1){
+          delete friendsList[i][key];
+        }
+      }
+    }
   });
 };
